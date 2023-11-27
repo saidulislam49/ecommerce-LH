670404923a8acb3d989e868791dd3e920b771b0e
saidulislam49 <saidulislam49du@gmail.com>
2023-11-27 10:32:04 +0600

    sldfjk

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,9 @@
 <?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('admin_route', function(){
-    return 'From Admin Route';
-});
+Route::get('/admin-login', [LoginController::class, 'adminLogin'])->name('admin.login');
+Route::get('/admin/home', [HomeController::class, 'admin'])->name('admin.home')->middleware('is_admin');
